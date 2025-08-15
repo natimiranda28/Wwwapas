279a1f162e8e104c786173af3d785d9e664b6293
Natalia Miranda <natumiranda.28@gmail.com>
2025-08-15 18:36:49 -0300

    etapa 5 y final

--- PHP/formulario.php
+++ PHP/formulario.php
@@ -1,26 +1,27 @@
- <?php
- $to = "info@hotmail.com";
- $subject = "Mail desde el formulario";
- $headers = "MIME-Version: 1.0" . "\r\n";
- $headers .= "Content-type:text/html;charset=UTF-8" . 
-"\r\n";
- $nombre = $_POST['nombre'];
- $email = $_POST['email'];
- $comentarios = $_POST['comentarios'];
- $message = "
- <html>
- <head>
- <title>HTML</title>
- </head>
- <body>
- <h1>Información del formulario</h1>
- <p>Nombre: $nombre </p> 
+<?php
+$to = "info@hotmail.com";
+$subject = "Mail desde el formulario";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$comentarios = $_POST['comentarios'];
+$menu = $_POST['menu'];
+$message = "
+<html>
+<head>
+<title>HTML</title>
+</head>
+<body>
+<h1>Información del formulario</h1>
+<p>Nombre: $nombre </p> 
 <p>Email: $email </p> 
 <p>Comentarios: $comentarios </p> 
+<p>Menú: $menu </p> 
 </body>
- </html>";
+</html>";
  echo 'Gracias por comunicarse con nosotros';
- mail($to, $subject, $message, $headers);
- ?>
+mail($to, $subject, $message, $headers);
+?>
 
  
